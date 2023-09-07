a37852a0fb647a446fe5c2bc7b14d25bc2c4af0c
김성인 <kimsungin5@gmail.com>
2023-09-07 17:08:15 +0900

    DOCS: backend 기본 설정 및 패키지명 변경

--- Backend/src/main/java/com/ssafy/backend/BackendApplication.java
+++ Backend/src/main/java/com/ssafy/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.ssafy.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Backend/src/test/java/com/ssafy/backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/ssafy/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.ssafy.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
