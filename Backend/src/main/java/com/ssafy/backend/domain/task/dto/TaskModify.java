a514fe148012046780fa0ea590b9e00a130de515
강동윤 <yty455@naver.com>
2023-09-12 11:28:54 +0900

    progress: 태스크 API 4

--- Backend/src/main/java/com/ssafy/backend/domain/task/dto/TaskModify.java
+++ Backend/src/main/java/com/ssafy/backend/domain/task/dto/TaskModify.java
@@ -1,4 +1,22 @@
 package com.ssafy.backend.domain.task.dto;
 
+import com.ssafy.backend.domain.task.Priority;
+import com.ssafy.backend.domain.task.Progress;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDate;
+
+@Getter
 public class TaskModify {
+    private Long taskGroupId; // -1이면 개인 태스크인 상태
+    private String name;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+    @Enumerated(EnumType.STRING)
+    private Progress progress;
+    private LocalDate deadline;
 }
--- Backend/src/main/java/com/ssafy/backend/domain/user/User.java
+++ Backend/src/main/java/com/ssafy/backend/domain/user/User.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.ssafy.backend.domain.task.Task;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.ssafy.backend.domain.common.BaseEntity;
@@ -29,64 +28,63 @@ import lombok.NoArgsConstructor;
 @Table(name = "USERS")
 public class User extends BaseEntity {
 
-	@Id
-	@GeneratedValue
-	@Column(name = "USER_ID")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "USER_ID")
+    private Long id;
 
-	private String socialId;
-	private String githubId;
-	private String nickname;
-	private String profileImage;
+    private String socialId;
+    private String githubId;
+    private String nickname;
+    private String profileImage;
 
-	private String email;
-	private String password;
+    private String email;
+    private String password;
 
-	@Enumerated(EnumType.STRING)
-	private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-	@Column(length = 500)
-	private String refreshToken;
+    @Column(length = 500)
+    private String refreshToken;
 
 
+    //== 유저 필드 업데이트 ==//
+    public void updateFirst(UserSignUpDto userSignUpDto) {
+        this.nickname = userSignUpDto.getNickname();
+    }
 
-	//== 유저 필드 업데이트 ==//
-	public void updateFirst(UserSignUpDto userSignUpDto) {
-		this.nickname = userSignUpDto.getNickname();
-	}
+    public void updateProfile(UserDto userDto) {
+        this.nickname = userDto.getNickname();
+        this.profileImage = userDto.getProfileImage();
+    }
 
-	public void updateProfile(UserDto userDto) {
-		this.nickname = userDto.getNickname();
-		this.profileImage = userDto.getProfileImage();
-	}
+    // 유저 권한 설정 메소드
+    public void authorizeUser() {
+        this.role = Role.USER;
+    }
 
-	// 유저 권한 설정 메소드
-	public void authorizeUser() {
-		this.role = Role.USER;
-	}
+    public void updateNickname(String updateNickname) {
+        this.nickname = updateNickname;
+    }
 
-	public void updateNickname(String updateNickname) {
-		this.nickname = updateNickname;
-	}
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
 
-	public void updateRefreshToken(String updateRefreshToken) {
-		this.refreshToken = updateRefreshToken;
-	}
+    // 비밀번호 암호화 메소드
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
 
-	// 비밀번호 암호화 메소드
-	public void passwordEncode(PasswordEncoder passwordEncoder) {
-		this.password = passwordEncoder.encode(this.password);
-	}
-
-	 @Override
-	 public String toString() {
-	     return "User{" +
-	             "id=" + id +
-	             ", socialId='" + socialId + '\'' +
-	             ", nickname='" + nickname + '\'' +
-	             ", githubId='" + githubId + '\'' +
-	             ", role=" + role +
-	             ", refreshToken='" + refreshToken + '\'' +
-	             '}';
-	 }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", socialId='" + socialId + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", githubId='" + githubId + '\'' +
+                ", role=" + role +
+                ", refreshToken='" + refreshToken + '\'' +
+                '}';
+    }
 }
