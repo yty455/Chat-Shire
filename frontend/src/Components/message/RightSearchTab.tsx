047c47a0e95305e75eb4ed379926f0234da47161
kdu201 <kdu201@naver.com>
2023-09-12 10:21:23 +0900

    fix: app.css 머징

--- frontend/src/Components/message/RightSearchTab.tsx
+++ frontend/src/Components/message/RightSearchTab.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './RightSearchTab.module.css'
 import TextField from '@mui/material/TextField';
+import ReactDOM from 'react-dom/client';
 
 export default function RightSearchTab() {
+
+  useEffect(() => {
+    const container = ReactDOM.createRoot(
+      document.getElementById('container') as HTMLElement
+    );
+    container.render(React.createElement('div', {class: 'gcse-search'}))
+  }, []) 
 
   return (
     <div className={styles.MessageRightBody}>
       <span className={styles.MessageRightBodyTitle}>
         검색
       </span>
-      <div className={styles.GoogleSearchContainer}>
-        <TextField style={{ marginTop: "10px", width: "500px"}} id="standard-basic" label="Google" variant="standard" />
+      <div id="container" className={styles.GoogleSearchContainer}>
       </div>
     </div>
   )
 }
+
