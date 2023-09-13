9cec9487f56ad41e276df72f56f43b832d2d906e
kdu201 <kdu201@naver.com>
2023-09-13 15:59:29 +0900

    feat: 메시지 페이지 북마크 구현 완

---
+++ frontend/src/Components/message/LinkOGItem.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import styles from './LinkOGItem.module.css'
+import Card from '@mui/material/Card';
+import { CardActionArea } from '@mui/material';
+
+
+export default function LinkOGItem() {
+  const [ title, setTitle ] = useState(null)
+  const [ desc, setDesc ] = useState(null)
+  const [ domain, setDomain ] = useState(null)
+  const [ favicon, setFavicon ] = useState(null)
+
+  const requestUrl = "https://www.npmjs.com/package/@dhaiwat10/react-link-preview"
+
+  const url = `https://jsonlink.io/api/extract?url=${requestUrl}`
+
+  axios.get(url)
+  .then((res) => {
+    setTitle(res.data.title)
+    setDesc(res.data.description)
+    setDomain(res.data.domain)
+    setFavicon(res.data.images[0])
+  })
+  
+  return (
+    <Card sx={{ borderRadius: 2, marginTop: 2}}>
+      <CardActionArea sx={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "flex-start", width: 460, height: 100, borderRadius: 2, padding: 2 }}>
+        <span className={styles.BookMarkTitle}>{title}</span>
+        <span className={styles.BookMarkDesc}>{desc}</span>
+        <div className={styles.BookMarkLink}>
+          <img className={styles.BookMarkFavicon} src={`${favicon}`} alt={`${title}`} />
+          <span>{domain}</span>
+        </div>
+      </CardActionArea>
+    </Card>
+  )
+}
