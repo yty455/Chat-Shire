4f2a76c8f3b80c4b049775c8b86399f0618478d2
강동윤 <yty455@naver.com>
2023-09-14 17:49:33 +0900

    feat: 채팅 레디스->sql 저장

--- Backend/src/main/java/com/ssafy/backend/domain/chat/dto/ChatInfo.java
+++ Backend/src/main/java/com/ssafy/backend/domain/chat/dto/ChatInfo.java
@@ -1,5 +1,8 @@
 package com.ssafy.backend.domain.chat.dto;
 
+import com.ssafy.backend.domain.chat.Chat;
+import com.ssafy.backend.domain.chat.entity.ChatRoom;
+import com.ssafy.backend.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,4 +17,12 @@ public class ChatInfo {
     private Long chatRoomId;
     private String content;
     private Long chatNumber;
+
+    public Chat toEntity(User user, ChatRoom chatRoom){
+        return Chat.builder()
+                .user(user)
+                .chatRoom(chatRoom)
+                .content(this.content)
+                .chatNumber(this.chatNumber).build();
+    }
 }
