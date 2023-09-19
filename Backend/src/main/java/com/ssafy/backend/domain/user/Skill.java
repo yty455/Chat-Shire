8f26aed50a3fb00ea4e0f53ed4a03663b08f60c8
강동윤 <yty455@naver.com>
2023-09-19 17:45:29 +0900

    progress: 회원 기술스택

---
+++ Backend/src/main/java/com/ssafy/backend/domain/user/Skill.java
@@ -0,0 +1,22 @@
+package com.ssafy.backend.domain.user;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Skill {
+    @Id
+    @GeneratedValue
+    @Column(name = "SKILL_ID")
+    private Long id;
+
+    private String skillname;
+}
---
+++ Backend/src/main/java/com/ssafy/backend/domain/user/repository/SkillRepository.java
@@ -0,0 +1,4 @@
+package com.ssafy.backend.domain.user.repository;
+
+public interface SkillRepository {
+}
