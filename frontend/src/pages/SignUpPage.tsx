dd928338b42b2322dd0ef018ab57285821724f21
최상익 <qjxltm@naver.com>
2023-09-20 09:04:53 +0900

    fix: 회원가입 페이지 인풋 추가

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function SignUpPage() {
+  const [formData, setFormData] = useState({
+    nickname: "",
+    profileImage: "",
+    profileColor: "",
+    introduction: "",
+    detailIntroduction: "",
+  });
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   const usersign = async () => {
     try {
-      const response = await axios.post("/api1/users", {
-        nickname: "bdc",
-        profileImage: "asd",
-        profileColor: "asd",
-        introduction: "asd",
-        detailIntroduction: "asd",
-      });
+      const response = await axios.post("/api1/users", formData);
       console.log(response.headers);
       localStorage.setItem("token", response.headers.Authorization);
       // sessionStorage.setItem("refreshtoken", response.headers.Authorization-Refresh);
@@ -23,6 +33,41 @@ function SignUpPage() {
 
   return (
     <div>
+      <input
+        type="text"
+        name="nickname"
+        value={formData.nickname}
+        onChange={handleInputChange}
+        placeholder="Nickname"
+      />
+      <input
+        type="text"
+        name="profileImage"
+        value={formData.profileImage}
+        onChange={handleInputChange}
+        placeholder="Profile Image"
+      />
+      <input
+        type="text"
+        name="profileColor"
+        value={formData.profileColor}
+        onChange={handleInputChange}
+        placeholder="Profile Color"
+      />
+      <input
+        type="text"
+        name="introduction"
+        value={formData.introduction}
+        onChange={handleInputChange}
+        placeholder="Introduction"
+      />
+      <input
+        type="text"
+        name="detailIntroduction"
+        value={formData.detailIntroduction}
+        onChange={handleInputChange}
+        placeholder="Detail Introduction"
+      />
       <button onClick={usersign}>회원가입</button>
     </div>
   );
