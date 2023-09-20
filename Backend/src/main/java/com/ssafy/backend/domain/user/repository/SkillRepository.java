ffa68d8c3c932464c8f54b1972c3de2927ab2b68
강동윤 <yty455@naver.com>
2023-09-20 11:58:28 +0900

    fix: SkillController null

--- Backend/src/main/java/com/ssafy/backend/domain/user/repository/SkillRepository.java
+++ Backend/src/main/java/com/ssafy/backend/domain/user/repository/SkillRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.backend.domain.user.repository;
 import com.ssafy.backend.domain.user.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+    List<Skill> findAll();
 }
