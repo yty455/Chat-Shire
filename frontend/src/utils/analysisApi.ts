ae2069d25e16497ebafeffed9c3cbe3a81cf1648
최상익 <qjxltm@naver.com>
2023-09-25 12:03:34 +0900

    feat: 통계 페이지 조회 api 작성, 개인 태스크 연결

--- frontend/src/utils/analysisApi.ts
+++ frontend/src/utils/analysisApi.ts
@@ -3,5 +3,5 @@ import api from "./api";
 //####### 통계
 
 // 통계 불러오기
-export const getAnalysis = (projectId: number) =>
+export const getAnalysis = (projectId: string) =>
   api.get(`/projects/${projectId}/statistic`);
