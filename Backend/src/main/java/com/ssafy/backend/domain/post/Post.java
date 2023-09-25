725cf60467d55441e82ad7208d8b8eab289fb600
강동윤 <yty455@naver.com>
2023-09-25 11:17:45 +0900

    feat: 에러게시판 CRUD

--- Backend/src/main/java/com/ssafy/backend/domain/post/Post.java
+++ Backend/src/main/java/com/ssafy/backend/domain/post/Post.java
@@ -2,10 +2,12 @@ package com.ssafy.backend.domain.post;
 
 import com.ssafy.backend.domain.chat.entity.ChatRoom;
 import com.ssafy.backend.domain.common.BaseEntity;
+import com.ssafy.backend.domain.post.dto.PostInfo;
 import com.ssafy.backend.domain.user.User;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.function.Consumer;
 
 import static javax.persistence.FetchType.LAZY;
 
@@ -31,5 +33,24 @@ public class Post extends BaseEntity {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "CHATROOM_ID")
     private ChatRoom chatRoom;
+
+    public void update(PostInfo postInfo) {
+        updateTitle(postInfo.getTitle());
+        updateContent(postInfo.getContent());
+    }
+
+    private <T> void updateIfNotNull(Consumer<T> updater, T newValue) {
+        if (newValue != null) {
+            updater.accept(newValue);
+        }
+    }
+
+    public void updateTitle(String title) {
+        updateIfNotNull(newValue -> this.title = newValue, title);
+    }
+
+    public void updateContent(String content) {
+        updateIfNotNull(newValue -> this.content = newValue, content);
+    }
 
 }
--- Backend/src/main/java/com/ssafy/backend/domain/post/service/PostService.java
+++ Backend/src/main/java/com/ssafy/backend/domain/post/service/PostService.java
@@ -14,6 +14,7 @@ import com.ssafy.backend.domain.post.repository.PostSkillRepository;
 import com.ssafy.backend.domain.post.repository.ReplyRepository;
 import com.ssafy.backend.domain.user.Skill;
 import com.ssafy.backend.domain.user.User;
+import com.ssafy.backend.domain.user.dto.MySkillInfo;
 import com.ssafy.backend.domain.user.exception.UserNotFoundException;
 import com.ssafy.backend.domain.user.repository.SkillRepository;
 import com.ssafy.backend.domain.user.repository.UserRepository;
@@ -21,8 +22,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.ssafy.backend.domain.common.GlobalMethod.getUserId;
@@ -48,6 +51,15 @@ public class PostService {
 
         return postInfoResponses;
     }
+    
+    public PostInfoDetailResponse getDetailPost(Long postId){
+        PostInfoDetailResponse postInfoDetailResponse = postRepository.getInfoById(postId);
+        List<String> skillName = postSkillRepository.findByPostId(postId);
+        postInfoDetailResponse.setSkillName(skillName);
+        List<ReplyInfoResponse> replyInfoResponses = replyRepository.findByPostId(postId);
+        postInfoDetailResponse.setReply(replyInfoResponses);
+        return postInfoDetailResponse;
+    }
 
     public Long registerPost(Long chatRoomId, PostInfo postInfo) {
         ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
@@ -68,17 +80,33 @@ public class PostService {
 
         return savedPost.getId();
     }
-    
-    public PostInfoDetailResponse getDetailPost(Long postId){
-        PostInfoDetailResponse postInfoDetailResponse = postRepository.getInfoById(postId);
-        List<String> skillName = postSkillRepository.findByPostId(postId);
-        postInfoDetailResponse.setSkillName(skillName);
-        List<ReplyInfoResponse> replyInfoResponses = replyRepository.findByPostId(postId);
-        postInfoDetailResponse.setReply(replyInfoResponses);
-        return postInfoDetailResponse;
-    }
 
     public void modifyPost(Long postId, PostInfo postInfo) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post.modifyPost", postId));
+        post.update(postInfo);
+
+        // 언어 스킬 재등록
+        Map<String, Skill> skillMap = skillRepository.findAll().stream()
+                .collect(Collectors.toMap(Skill::getSkillName, skill -> skill));
+        // 내가 등록한 언어 스킬
+        List<MySkillInfo> mySkills = postSkillRepository.getPostSkill(postId);
+        // 수정한 언어 목록
+        Set<String> modifySkillSet = new HashSet<>(postInfo.getSkillName());
+
+        for (MySkillInfo mySkill : mySkills) {
+            if (modifySkillSet.contains(mySkill.getSkillName())) {
+                modifySkillSet.remove(mySkill.getSkillName());
+            } else {
+                postSkillRepository.deleteById(mySkill.getId());
+            }
+        }
+
+        for (String name : modifySkillSet) {
+            postSkillRepository.save(PostSkill.builder()
+                    .skill(skillMap.get(name))
+                    .post(post).build());
+        }
     }
 
 
