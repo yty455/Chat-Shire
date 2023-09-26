9b9124f9c49ee116fea6d266d2973c2dff7d2582
이상훈 <hoonideal@naver.com>
2023-09-26 15:23:52 +0900

    feat: pyspark를 활용한 분산처리 test

--- Backend/src/main/java/com/ssafy/backend/domain/chat/controller/ChatSchedulerContoller.java
+++ Backend/src/main/java/com/ssafy/backend/domain/chat/controller/ChatSchedulerContoller.java
@@ -2,6 +2,7 @@ package com.ssafy.backend.domain.chat.controller;
 
 import com.ssafy.backend.domain.chat.service.ChatScheduler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,11 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 
+@RequestMapping("distributed")
 public class ChatSchedulerContoller {
     final private ChatScheduler chatScheduler;
 
-    @RequestMapping("distributed")
+    @GetMapping("/start")
     public String distributed() throws IOException {
         chatScheduler.chatTransfer();
         chatScheduler.sparkStart();
