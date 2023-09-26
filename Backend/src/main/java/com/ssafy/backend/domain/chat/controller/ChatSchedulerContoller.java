e29d528937399706586819c9549aa141c5ecbe47
이상훈 <hoonideal@naver.com>
2023-09-26 15:57:44 +0900

    feat: pyspark를 활용한 분산처리 test

--- Backend/src/main/java/com/ssafy/backend/domain/chat/controller/ChatSchedulerContoller.java
+++ Backend/src/main/java/com/ssafy/backend/domain/chat/controller/ChatSchedulerContoller.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 
-@RequestMapping("distributed")
+@RequestMapping("/distributed")
 public class ChatSchedulerContoller {
     final private ChatScheduler chatScheduler;
 
