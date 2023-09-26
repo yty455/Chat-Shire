bbb47880370c709848544f8657c33a8b3654728e
강동윤 <yty455@naver.com>
2023-09-26 17:03:33 +0900

    fix: 첨부파일 Repo - 서랍 조회

--- Backend/src/main/java/com/ssafy/backend/domain/attachedFile/repository/AttachedFileRepository.java
+++ Backend/src/main/java/com/ssafy/backend/domain/attachedFile/repository/AttachedFileRepository.java
@@ -18,7 +18,8 @@ public interface AttachedFileRepository extends JpaRepository<AttachedFile, Long
     @Query("select new com.ssafy.backend.domain.attachedFile.dto.AttachedFileInfo(a.url, a.thumbnail) from AttachedFile a where a.chatRoomId = :chatRoomId and a.chatNumber = :chatNumber")
     List<AttachedFileInfo> findByChatRoomIdAndChatNumber(@Param("chatRoomId") Long chatRoomId, @Param("chatNumber") Long chatNumber);
 
-    List<AttachedFileInfo> findInfoByChatRoomIdAndCategory(@Param("chatRoomId") Long chatRoomId, @Param("Category") Category category);
+    @Query("select new com.ssafy.backend.domain.attachedFile.dto.AttachedFileInfo(a.url, a.thumbnail) from AttachedFile a where a.chatRoomId = :chatRoomId and a.category = :category")
+    List<AttachedFileInfo> findInfoByChatRoomIdAndCategory(@Param("chatRoomId") Long chatRoomId, @Param("category") Category category);
 
     void deleteAllByPostId(Long postId);
 }
