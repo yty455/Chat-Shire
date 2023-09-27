dea01f95317448d66dba9f5f9c37c879012c8afe
김성인 <kimsungin5@gmail.com>
2023-09-27 12:18:17 +0900

    feat: 초대 수락, 거절 기능 추가

--- Backend/src/main/java/com/ssafy/backend/domain/chat/controller/InvitationController.java
+++ Backend/src/main/java/com/ssafy/backend/domain/chat/controller/InvitationController.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.backend.domain.chat.dto.MyInvitationResponse;
@@ -23,7 +25,7 @@ public class InvitationController {
 
 	private final InvitationService invitationService;
 	@Operation(summary = "초대 조회", description = "내가 받은 초대를 조회합니다.")
-	@GetMapping("/invitation")
+	@GetMapping("/invitations")
 	public ResponseEntity<BasicResponse> getMyInvitation() {
 		List<MyInvitationResponse> myInvitations = invitationService.getMyInvitation().stream()
 				.map(MyInvitationResponse::toDto)
@@ -33,6 +35,30 @@ public class InvitationController {
 				.message("내가 받은 초대 조회 성공")
 				.count(myInvitations.size())
 				.result(Collections.singletonList(myInvitations))
+				.build();
+
+		return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
+	}
+
+	@Operation(summary = "초대 수락", description = "내가 받은 초대를 수락합니다.")
+	@PatchMapping("/invitations/{invitationId}/accept")
+	public ResponseEntity<BasicResponse> acceptInvitation(@PathVariable Long invitationId) {
+		invitationService.acceptInvitation(invitationId);
+
+		BasicResponse basicResponse = BasicResponse.builder()
+				.message("초대 수락 성공")
+				.build();
+
+		return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
+	}
+
+	@Operation(summary = "초대 거절", description = "내가 받은 초대를 거절합니다.")
+	@PatchMapping("/invitations/{invitationId}/reject")
+	public ResponseEntity<BasicResponse> rejectInvitation(@PathVariable Long invitationId) {
+		invitationService.rejectInvitation(invitationId);
+
+		BasicResponse basicResponse = BasicResponse.builder()
+				.message("초대 거절 성공")
 				.build();
 
 		return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
--- Backend/src/main/java/com/ssafy/backend/domain/chat/service/InvitationService.java
+++ Backend/src/main/java/com/ssafy/backend/domain/chat/service/InvitationService.java
@@ -9,7 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.backend.domain.chat.entity.AcceptanceStatus;
 import com.ssafy.backend.domain.chat.entity.Notification;
+import com.ssafy.backend.domain.chat.entity.Participation;
 import com.ssafy.backend.domain.chat.repository.NotificationRepository;
+import com.ssafy.backend.domain.chat.repository.ParticipationRepository;
+import com.ssafy.backend.domain.common.exception.ResourceNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,8 +22,28 @@ import lombok.RequiredArgsConstructor;
 public class InvitationService {
 
 	private final NotificationRepository notificationRepository;
+	private final ParticipationRepository participationRepository;
 
 	public List<Notification> getMyInvitation() {
 		return notificationRepository.findAllByReceiverIdAndStatus(getUserId(), AcceptanceStatus.NOT_READ);
+	}
+
+	@Transactional
+	public void acceptInvitation(Long invitationId) {
+		Notification notification = notificationRepository.findById(invitationId)
+				.orElseThrow(() -> new ResourceNotFoundException("Notification", invitationId));
+		notification.accept();
+
+		participation(notification);
+	}
+
+	private void participation(Notification notification) {
+		participationRepository.save(Participation.create(notification.getReceiver(), notification.getParticipation().getChatRoom()));
+	}
+
+	public void rejectInvitation(Long invitationId) {
+		Notification notification = notificationRepository.findById(invitationId)
+				.orElseThrow(() -> new ResourceNotFoundException("Notification", invitationId));
+		notification.reject();
 	}
 }
