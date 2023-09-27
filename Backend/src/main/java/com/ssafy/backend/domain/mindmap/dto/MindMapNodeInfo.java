b0ee5dfd31a2111f89dfdbb1d74d308ae2e3d859
이상훈 <hoonideal@naver.com>
2023-09-27 15:54:23 +0900

    feat: mindmap 구현 중

---
+++ Backend/src/main/java/com/ssafy/backend/domain/mindmap/dto/MindMapNodeInfo.java
@@ -0,0 +1,35 @@
+package com.ssafy.backend.domain.mindmap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MindMapNodeInfo {
+	private Long mindMapId;
+	private Integer id;
+	private Data data;
+	private Position position;
+	private String parentNode;
+
+	@Builder
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Data {
+		private String label;
+	}
+
+	@Builder
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Position{
+		private Double x;
+		private Double y;
+	}
+}
--- Backend/src/main/java/com/ssafy/backend/domain/mindmap/service/MindMapService.java
+++ Backend/src/main/java/com/ssafy/backend/domain/mindmap/service/MindMapService.java
@@ -3,10 +3,11 @@ package com.ssafy.backend.domain.mindmap.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.ssafy.backend.domain.mindmap.MindMap;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ssafy.backend.domain.mindmap.dto.MindMapNodes;
+import com.ssafy.backend.domain.mindmap.dto.MindMapNodeInfo;
 import com.ssafy.backend.domain.mindmap.repository.MindMapRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,15 +18,29 @@ import lombok.RequiredArgsConstructor;
 public class MindMapService {
 	private final MindMapRepository mindMapRepository;
 
-	public List<MindMapNodes> getMindMap(Long chatRoomId) {
+	public List<MindMapNodeInfo> getMindMap(Long chatRoomId) {
 		return mindMapRepository.findByChatRoomId(chatRoomId).stream()
-				.map(chatroom -> MindMapNodes.builder()
-						.id(chatroom.getId())
-						.nodeId(chatroom.getNodeId())
-						.x(chatroom.getX())
-						.y(chatroom.getY())
-						.content(chatroom.getContent()).build()).
+				.map(chatroom -> MindMapNodeInfo.builder()
+						.mindMapId(chatroom.getId())
+						.id(chatroom.getNodeId())
+						.position(MindMapNodeInfo.Position.builder().x(chatroom.getX()).y(chatroom.getY()).build())
+						.data(MindMapNodeInfo.Data.builder().label(chatroom.getContent()).build()).build()).
 				collect(Collectors.toList());
 	}
+
+//	@Transactional
+//	public void saveMinMap(Long chatRoomId, List<MindMapNodeInfo> mindMapNodes) {
+//		// chatRoomId로 기존의 mindMap 삭제
+//		mindMapRepository.deleteByChatRoomId(chatRoomId);
+//
+//		// mindMapNodes를 모두 mindMap에 저장
+//		for (MindMapNodeInfo node: mindMapNodes
+//			 ) {
+//			mindMapRepository.save(MindMap.builder()
+//					.id(node.getId())
+//					.
+//					)
+//		}
+//	}
 }
 
