528d282f087cf6dae537abc9034c956065fee34a
강동윤 <yty455@naver.com>
2023-10-03 23:07:17 +0900

    fix: 채팅방 나가기 - 내가 보낸 초대 지우고 참여 삭제

--- Backend/src/main/java/com/ssafy/backend/domain/chat/repository/NotificationRepository.java
+++ Backend/src/main/java/com/ssafy/backend/domain/chat/repository/NotificationRepository.java
@@ -1,14 +1,16 @@
 package com.ssafy.backend.domain.chat.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ssafy.backend.domain.chat.entity.AcceptanceStatus;
 import com.ssafy.backend.domain.chat.entity.Notification;
 import com.ssafy.backend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-	List<Notification> findAllByReceiver(User user);
-	List<Notification> findAllByReceiverIdAndStatus(Long receiverId, AcceptanceStatus status);
+    List<Notification> findAllByReceiver(User user);
+
+    List<Notification> findAllByReceiverIdAndStatus(Long receiverId, AcceptanceStatus status);
+
+    void deleteAllByParticipationId(Long chatRoomId);
 }
