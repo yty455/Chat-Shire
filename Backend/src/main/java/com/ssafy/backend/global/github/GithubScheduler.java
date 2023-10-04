d1218db58ba54e5114da41757844454493603c8e
강동윤 <yty455@naver.com>
2023-10-04 16:52:13 +0900

    fix: 내정보 조회 - 개인 통계데이터 추출

--- Backend/src/main/java/com/ssafy/backend/global/github/GithubScheduler.java
+++ Backend/src/main/java/com/ssafy/backend/global/github/GithubScheduler.java
@@ -24,60 +24,59 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class GithubScheduler {
 
-	private final ChatRoomRepository chatRoomRepository;
-	private final StatisticService statisticService;
-	private final ChallengeService challengeService;
-	private final GithubApi githubApi;
+    private final ChatRoomRepository chatRoomRepository;
+    private final StatisticService statisticService;
+    private final ChallengeService challengeService;
+    private final GithubApi githubApi;
 
-	@Scheduled(cron = "0 * * * * ?")
-	public void countCommitHistory() throws IOException {
-		System.out.println("커밋 히스토리 가져오기 실행");
-		LocalDate today = LocalDate.now();
-		List<ChatRoom> chatRooms = chatRoomRepository.findByDate(today);
+    @Scheduled(cron = "0 * * * * ?")
+    public void countCommitHistory() throws IOException {
+        System.out.println("커밋 히스토리 가져오기 실행");
+        LocalDate today = LocalDate.now();
+        List<ChatRoom> chatRooms = chatRoomRepository.findByDate(today);
 
-		for (ChatRoom chatRoom : chatRooms) {
-			if (chatRoom.getGitRepository() == null || chatRoom.getBranch() == null
-					|| chatRoom.getGitAccessToken() == null) {
-				continue;
-			}
-			Long morningCommitCount = 0L;
-			Long afternoonCommitCount = 0L;
-			Long nightCommitCount = 0L;
+        for (ChatRoom chatRoom : chatRooms) {
+            if (chatRoom.getGitRepository() == null || chatRoom.getBranch() == null
+                    || chatRoom.getGitAccessToken() == null) {
+                continue;
+            }
+            Long morningCommitCount = 0L;
+            Long afternoonCommitCount = 0L;
+            Long nightCommitCount = 0L;
 
-			System.out.println("chatRoom.getGitRepository() = " + chatRoom.getGitRepository());
-			System.out.println("chatRoom.getBranch() = " + chatRoom.getBranch());
-			Map<String, List<Date>> commitDatesSince = githubApi.getCommitDatesSince(chatRoom.getGitRepository(), chatRoom.getBranch(), chatRoom.getGitAccessToken());
+            System.out.println("chatRoom.getGitRepository() = " + chatRoom.getGitRepository());
+            System.out.println("chatRoom.getBranch() = " + chatRoom.getBranch());
+            Map<String, List<Date>> commitDatesSince = githubApi.getCommitDatesSince(chatRoom.getGitRepository(), chatRoom.getBranch(), chatRoom.getGitAccessToken());
 
-			// ksi2564 : [커밋한 시간1, 커밋한 시간2]...
-			for (String githubId : commitDatesSince.keySet()) {
-				Map<String, Long> counts = commitDatesSince.get(githubId).stream()
-						.map(date -> date.toInstant().atZone(ZoneId.systemDefault()).toLocalTime())
-						.map(time -> {
-							if (!time.isBefore(LocalTime.of(4, 0))
-									&& time.isBefore(LocalTime.of(12, 0))) {
-								return "morning";
-							} else if (!time.isBefore(LocalTime.of(12, 0))
-									&& time.isBefore(LocalTime.of(20, 0))) {
-								return "afternoon";
-							} else {
-								return "night";
-							}
-						})
-						.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            // ksi2564 : [커밋한 시간1, 커밋한 시간2]...
+            for (String githubId : commitDatesSince.keySet()) {
+                Map<String, Long> counts = commitDatesSince.get(githubId).stream()
+                        .map(date -> date.toInstant().atZone(ZoneId.systemDefault()).toLocalTime())
+                        .map(time -> {
+                            if (!time.isBefore(LocalTime.of(4, 0))
+                                    && time.isBefore(LocalTime.of(12, 0))) {
+                                return "morning";
+                            } else if (!time.isBefore(LocalTime.of(12, 0))
+                                    && time.isBefore(LocalTime.of(20, 0))) {
+                                return "afternoon";
+                            } else {
+                                return "night";
+                            }
+                        })
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-				long myCommitCount = counts.get("morning") + counts.get("afternoon") + counts.get("night");
-				// 유저의 도전과제 commit 수 추가 함
-				challengeService.updateMyCommit(Long.valueOf(githubId), myCommitCount);
+                // 유저의 도전과제 commit 수 추가 함
+                challengeService.updateMyCommit(githubId, counts.get("morning"), counts.get("afternoon"), counts.get("night"));
 
-				morningCommitCount += counts.get("morning");
-				afternoonCommitCount += counts.get("afternoon");
-				nightCommitCount += counts.get("night");
-			}
+                morningCommitCount += counts.get("morning");
+                afternoonCommitCount += counts.get("afternoon");
+                nightCommitCount += counts.get("night");
+            }
 
-			// 프로젝트 관련 통계에 업데이트하기
-			statisticService.updateCommitCount(chatRoom.getId(), morningCommitCount, afternoonCommitCount,
-					nightCommitCount);
-		}
+            // 프로젝트 관련 통계에 업데이트하기
+            statisticService.updateCommitCount(chatRoom.getId(), morningCommitCount, afternoonCommitCount,
+                    nightCommitCount);
+        }
 
-	}
+    }
 }
