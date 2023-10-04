6e72b0409f9f1f08f1d5fc08064a969e13760f0a
김성인 <kimsungin5@gmail.com>
2023-10-04 16:10:46 +0900

    feat: github 스케줄러 없애기

--- Backend/src/main/java/com/ssafy/backend/global/github/GithubScheduler.java
+++ Backend/src/main/java/com/ssafy/backend/global/github/GithubScheduler.java
@@ -57,6 +57,7 @@ public class GithubScheduler {
 			// ksi2564 : [커밋한 시간1, 커밋한 시간2]...
 			for (String githubId : commitDatesSince.keySet()) {
 				System.out.println(commitDatesSince.get(githubId).size());
+				System.out.println(commitDatesSince.get(githubId));
 				Map<String, Long> counts = commitDatesSince.get(githubId).stream()
 						.map(date -> date.toInstant().atZone(ZoneId.systemDefault()).toLocalTime())
 						.map(time -> {
