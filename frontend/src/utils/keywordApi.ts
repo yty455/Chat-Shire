c9c35fc92854d07f95817dc91a7dfb243865b6f2
csi <qjxltm@naver.com>
2023-10-08 04:42:50 +0900

    fix:오류수정

--- frontend/src/utils/keywordApi.ts
+++ frontend/src/utils/keywordApi.ts
@@ -6,7 +6,7 @@ import api from "./api";
 export const getKeyword = (projectId:string) => api.get(`/projects/${projectId}/keywords`);
 
 // 키워드 추가
-export const postKeyword = (projectId:string, word:string[]) => api.post(`/projects/${projectId}/keywords`, {word});
+export const postKeyword = (projectId:string, words:string[]) => api.post(`/projects/${projectId}/keywords`, {words});
 
 // 키워드 삭제
-export const deleteKeyword = (projectId:string, word:string[]) => api.delete(`/projects/${projectId}/keywords`, {data: { word }});
+export const deleteKeyword = (projectId:string, words:string[]) => api.delete(`/projects/${projectId}/keywords`, {data: { words }});
