2db726b0402167b7e9b2a86c6362794eb37e589b
gayoung <lgy4625@naver.com>
2023-10-09 15:08:59 +0900

    A

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -7,7 +7,7 @@ import BarChart from "../components/analysis/BarChart";
 import LeftSide from "../components/common/LeftSide";
 import { getProfile } from "../utils/userApi";
 import { loginuser } from "../stores/atom";
-import { BsGithub, BsPersonFill } from "react-icons/bs";
+import { BsGithub, BsPersonFill, BsEmojiSmileFill } from "react-icons/bs";
 // import {
 //   BiLogoTypescript,
 //   BiLogoJavascript,
@@ -117,7 +117,7 @@ export default function ProfilePage() {
           <div className={styles.profileHeaderDesc}>
             <div className={styles.profileHeaderLeft}>
               <span className={styles.profileName}>
-                {userData?.nickname} {userData?.position}
+                {userData?.nickname} <span className={styles.profilePos}>{userData?.position}</span>
               </span>
               <div className={styles.profileCareer}>
                 <div className={styles.profileCareerItem}>
@@ -126,7 +126,7 @@ export default function ProfilePage() {
                 </div>
                 <div className={styles.profileCareerItem}>
                   {userData?.mySkill?.map((item: any) => (
-                    <span key={item}>{item} &nbsp;</span>
+                    <span className={styles.career} key={item}>{item} &nbsp;</span>
                   ))}
                 </div>
                 <div className={styles.profileCareerItem}>
@@ -135,6 +135,7 @@ export default function ProfilePage() {
                 </div>
               </div>
               <div className={styles.profileIntroduce}>
+                <BsEmojiSmileFill size={30} style={{ marginRight: "8px" }} />
                 <span>{userData?.detailIntroduction}</span>
               </div>
             </div>
@@ -158,15 +159,15 @@ export default function ProfilePage() {
         {userData && (
           <div className={styles.profileBody}>
             <div className={styles.profileBodyTitle}>
-              <span style={{ color: "#575757", fontSize: "34px" }}>
-                Achievement{" "}
+              <span style={{ fontFamily:'preRg', color: "#575757", fontSize: "30px" }}>
+                나의 기록{" "}
               </span>
-              <span style={{ color: "#575757", fontSize: "20px" }}>
+              <span style={{ fontFamily: 'preBd', color: "#575757", fontSize: "20px" }}>
                 {userData?.challengeInfoResponse.done}/36
               </span>
             </div>
             <div className={styles.progressBar}>
-              <Box sx={{ width: "400px" }}>
+              <Box sx={{ width: "200px" }}>
                 <LinearProgressWithLabel
                   value={(userData?.challengeInfoResponse.done / 36) * 100}
                 />
