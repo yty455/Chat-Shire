d3fccc650eb787331e82f469f4e7bb70d3b3b586
dang417 <dndeo10814@gmail.com>
2023-10-09 19:21:59 +0900

    feat: 태스크 모달 정

--- frontend/src/components/common2/TaskModal.tsx
+++ frontend/src/components/common2/TaskModal.tsx
@@ -99,7 +99,7 @@ function TaskModal({
               <span style={{fontFamily: "preBd", fontSize: "24px", marginBottom: "20px"}} onClick={() => handleEditClick("name")}>
                 <TextField
                   onClick={() => handleEditClick("name")}
-                  sx={{ width: "90%", margin: "14px 0px 0px -2px" }}
+                  sx={{ width: "340px", margin: "14px 0px 0px -2px" }}
                   color="greenary"
                   variant="standard"
                   multiline
@@ -194,7 +194,7 @@ function TaskModal({
             <span style={{fontFamily: "preRg", fontSize: "16px", marginBottom: "20px"}} onClick={() => handleEditClick("description")}>
               <TextField
                 onClick={() => handleEditClick("description")}
-                sx={{ width: "100%", margin: "14px 0px 0px -2px" }}
+                sx={{ width: "340px", margin: "14px 0px 0px -2px" }}
                 color="greenary"
                 // variant="standard"
                 multiline
