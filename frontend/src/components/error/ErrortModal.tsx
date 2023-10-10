e08e6c71bc52b14d64e9053cf92a0ebe33816067
최상익 <qjxltm@naver.com>
2023-10-11 08:50:35 +0900

    fix: 에러 모달 오류 수정

--- frontend/src/components/error/ErrortModal.tsx
+++ frontend/src/components/error/ErrortModal.tsx
@@ -15,7 +15,7 @@ import { Button } from "antd";
 
 import { BsPencilFill } from "react-icons/bs";
 import { MdDelete, MdOutlineCancel } from "react-icons/md";
-import { AiOutlineDownload } from 'react-icons/ai'
+import { AiOutlineDownload } from "react-icons/ai";
 import api from "../../utils/api";
 
 interface ErrorModalProps {
@@ -49,7 +49,7 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
       if (pjtId) {
         const response = await getErrors(pjtId);
         setAllErr(response.data.result[0]);
-        setAnswer(response.data.result[0].state)
+        setAnswer(response.data.result[0].state);
       }
     } catch (error) {
       console.error(error);
@@ -109,15 +109,16 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
     }
   };
 
-  const selectAnswer = async(id: number) => {
-    const patchedErrPost = errDetail
-    patchedErrPost.state = id
-    api.patch(`/posts/${errDetail.id}`, patchedErrPost)
-    .then((res) => {
-      console.log(res)
-      setAnswer(id)
-    })
-  }
+  const selectAnswer = async (id: number) => {
+    const patchedErrPost = errDetail;
+    patchedErrPost.state = id;
+    api.patch(`/posts/${errDetail.id}`, patchedErrPost).then((res) => {
+      console.log(res);
+      setAnswer(id);
+      getInErrors();
+      // getInError();
+    });
+  };
 
   // 엔터 키 입력 시 댓글 작성
   const handleEnterKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -137,7 +138,7 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
 
   useEffect(() => {
     getInError();
-  }, []);
+  }, [answer]);
 
   return (
     <div className={styles.modalOverlay}>
@@ -160,7 +161,7 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
                   ? formatChatTime(errDetail.lastModifiedDate)
                   : "날짜 없음"}
               </span>
-              <span style={{ marginLeft: "28px" , fontSize: "16px"}}>
+              <span style={{ marginLeft: "28px", fontSize: "16px" }}>
                 작성자 {errDetail.nickname}
               </span>
             </div>
@@ -183,7 +184,7 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
                           marginRight: "16px",
                           maxHeight: "280px",
                           height: "280px",
-                          borderRadius: "10px"
+                          borderRadius: "10px",
                         }}
                         onClick={ErrorImageClickHandler}
                         key={index}
@@ -191,7 +192,10 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
                         alt="Preview"
                       />
                       <div className={styles.hoverOverlay}>
-                        <AiOutlineDownload onClick={() => window.open(info.url, "_blank")} className={styles.downButton}/>
+                        <AiOutlineDownload
+                          onClick={() => window.open(info.url, "_blank")}
+                          className={styles.downButton}
+                        />
                       </div>
                     </div>
                   )
@@ -258,11 +262,15 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
                             />
                           </div>
                         ) : null}
-                        {userData.nickname === errDetail.nickname && item.nickname !== errDetail.nickname ? (
+                        {userData.nickname === errDetail.nickname &&
+                        item.nickname !== errDetail.nickname ? (
                           errDetail.state === item.replyId ? (
                             <Button
                               onClick={() => selectAnswer(0)}
-                              style={{ backgroundColor: "#39a789", fontFamily: "preRg" }}
+                              style={{
+                                backgroundColor: "#39a789",
+                                fontFamily: "preRg",
+                              }}
                               key="submit"
                               type="primary"
                             >
@@ -271,7 +279,10 @@ function ErrorModal({ pjtId, closeModal, err }: ErrorModalProps) {
                           ) : (
                             <Button
                               onClick={() => selectAnswer(item.replyId)}
-                              style={{ backgroundColor: "#39a789", fontFamily: "preRg" }}
+                              style={{
+                                backgroundColor: "#39a789",
+                                fontFamily: "preRg",
+                              }}
                               key="submit"
                               type="primary"
                             >
