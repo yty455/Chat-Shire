727e7d6807944cfc43061528ff671d94d190d844
gayoung <lgy4625@naver.com>
2023-10-10 23:35:39 +0900

    indivtask modal

--- frontend/src/components/common2/IndivChatModal.tsx
+++ frontend/src/components/common2/IndivChatModal.tsx
@@ -15,6 +15,7 @@ interface IndivChatModalProps {
   onClose: () => void;
   taskId: any;
   projectId: string;
+  open: boolean;
 }
 interface ChatItem {
   userId?: string;
@@ -29,7 +30,7 @@ interface Member {
   nickname: string;
 }
 
-function IndivChatModal({ taskId, onClose, projectId }: IndivChatModalProps) {
+function IndivChatModal({ taskId, onClose, projectId, open }: IndivChatModalProps) {
   const [taskChat, setTaskChat] = useState<ChatItem[]>([]);
   const [reChat, setReChat] = useState<ChatItem[]>([]);
   const [chat, setChat] = useState([]);
@@ -93,7 +94,16 @@ function IndivChatModal({ taskId, onClose, projectId }: IndivChatModalProps) {
   }, [taskId]);
 
   return (
-    <div className={styles.modalOverlay}>
+    <div>
+      <Modal
+        title="참조된 채팅"
+        centered
+        open={open}
+        onOk={onClose}
+        onCancel={onClose}
+        footer={null}
+      >
+    {/* <div className={styles.modalOverlay}> */}
       {selectedChat !== "" ? (
         <div className={styles.modalBox}>
           {reChat &&
@@ -135,10 +145,7 @@ function IndivChatModal({ taskId, onClose, projectId }: IndivChatModalProps) {
             taskChat.map((chat) => (
               <div key={chat.chatNumber} className={styles.chat}>
                 {" "}
-                <div className={styles.nickname}>{chat.nickname} : </div>
-                <div className={styles.content}>
-                  {chat.content ? chat.content : "첨부파일"}
-                </div>
+                <div className={styles.nickname}>{chat.nickname} : {chat.content ? chat.content : "첨부파일"}</div>
                 <div className={styles.chatTime}>
                   {" "}
                   {formatChatTime(chat.chatTime)}
@@ -191,6 +198,7 @@ function IndivChatModal({ taskId, onClose, projectId }: IndivChatModalProps) {
             {/* </div> */}
         </div>
       )}
+      </Modal>
     </div>
   );
 }
