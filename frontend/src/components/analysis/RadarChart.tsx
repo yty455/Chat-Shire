3dda50a67ba5d51e19b61382cf9fe003c1f5eeed
최상익 <qjxltm@naver.com>
2023-10-11 16:22:23 +0900

    fix: 오류수정

--- frontend/src/components/analysis/RadarChart.tsx
+++ frontend/src/components/analysis/RadarChart.tsx
@@ -81,63 +81,70 @@ const RadarChart = () => {
   const data = [
     {
       skill: "개발",
-      chardonay: minMaxScaling(
-        morningCommit + afternoonCommit + nightCommit + maxValue * 0.3
-      ),
-      // morningCommit+afternoonCommit+nightCommit+ maxValue* 0.2,
+      chardonay: morningCommit + afternoonCommit + nightCommit + maxValue * 0.3,
+      //   minMaxScaling(
+      //     morningCommit + afternoonCommit + nightCommit + maxValue * 0.3
+      //   ),
+
       // "chardonay": 8,
     },
     {
       skill: "디버깅",
-      chardonay: minMaxScaling(issueCount + maxValue * 0.3),
-      // issueCount+ maxValue* 0.2,
+      chardonay: issueCount + maxValue * 0.3,
+      //   minMaxScaling(issueCount + maxValue * 0.3),
+
       // "chardonay": 7,
     },
     {
       skill: "분위기 메이커",
-      chardonay: minMaxScaling(
-        totalChatCount(allCategoryCount) + maxValue * 0.3
-      ),
-      // totalChatCount(allCategoryCount)+ maxValue* 0.2,
+      chardonay: totalChatCount(allCategoryCount) + maxValue * 0.3,
+      //   minMaxScaling(
+      //     totalChatCount(allCategoryCount) + maxValue * 0.3
+      //   ),
+
       // "chardonay": 10,
     },
     {
       skill: "협업 의지",
-      chardonay: minMaxScaling(
-        relevantChatCount(allCategoryCount) + maxValue * 0.3
-      ),
-      // relevantChatCount(allCategoryCount)+ maxValue* 0.2,
+      chardonay: relevantChatCount(allCategoryCount) + maxValue * 0.3,
+      //   minMaxScaling(
+      //     relevantChatCount(allCategoryCount) + maxValue * 0.3
+      //   ),
+
       // "chardonay": 7,
     },
     {
       skill: "일정 관리",
-      chardonay: minMaxScaling(taskCount + maxValue * 0.3),
-      // taskCount+ maxValue* 0.2,
+      chardonay: taskCount + maxValue * 0.3,
+      //   minMaxScaling(taskCount + maxValue * 0.3),
+
       // "chardonay": 8,
     },
   ];
 
   return (
-    <ResponsiveRadar
-      theme={{ fontFamily: "preLt", fontSize: 18, textColor: "#ffffff" }}
-      data={data}
-      keys={["chardonay"]}
-      indexBy="skill"
-      valueFormat=" >-.2f"
-      margin={{ top: 40, right: 40, bottom: 40, left: 80 }}
-      borderWidth={0}
-      borderColor={{ from: "color", modifiers: [] }}
-      gridLevels={3}
-      gridLabelOffset={24}
-      enableDots={false}
-      dotSize={2}
-      dotColor={{ theme: "background" }}
-      dotLabelYOffset={-14}
-      colors={[workStyleColor.sub]}
-      fillOpacity={0.6}
-      blendMode="normal"
-      motionConfig="default"
-    />
+    <div>
+      <ResponsiveRadar
+        theme={{ fontFamily: "preLt", fontSize: 18, textColor: "#ffffff" }}
+        data={data}
+        keys={["chardonay"]}
+        indexBy="skill"
+        valueFormat=" >-.2f"
+        margin={{ top: 40, right: 40, bottom: 40, left: 80 }}
+        borderWidth={0}
+        borderColor={{ from: "color", modifiers: [] }}
+        gridLevels={3}
+        gridLabelOffset={24}
+        enableDots={false}
+        dotSize={2}
+        dotColor={{ theme: "background" }}
+        dotLabelYOffset={-14}
+        colors={[workStyleColor.sub]}
+        fillOpacity={0.6}
+        blendMode="normal"
+        motionConfig="default"
+      />
+    </div>
   );
 };
 
