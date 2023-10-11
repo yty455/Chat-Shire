c2ae52990f06a0b46516948c523414bef1f446a0
csi <qjxltm@naver.com>
2023-10-11 21:05:35 +0900

    fix: 통계 수정

--- frontend/src/components/analysis/RadarChart.tsx
+++ frontend/src/components/analysis/RadarChart.tsx
@@ -78,40 +78,54 @@ const RadarChart = () => {
     return (num - minVal) / (maxVal - minVal);
   }
 
+  function logScaling(num: number): number {
+    // Math.log1p(x) returns the natural logarithm (base e) of 1 + x
+    return Math.log1p(num);
+ }
+
   const data = [
     {
       skill: "개발",
-      chardonay: minMaxScaling(
-        morningCommit + afternoonCommit + nightCommit + maxValue * 0.3
-      ),
+      chardonay: logScaling( morningCommit + afternoonCommit + nightCommit),
+    //   minMaxScaling(
+    //     morningCommit + afternoonCommit + nightCommit + maxValue * 0.3
+    //   ),
       // morningCommit+afternoonCommit+nightCommit+ maxValue* 0.2,
       // "chardonay": 8,
     },
     {
       skill: "디버깅",
-      chardonay: minMaxScaling(issueCount + maxValue * 0.3),
+      chardonay: logScaling(issueCount),
+    //   minMaxScaling(issueCount + maxValue * 0.3),
       // issueCount+ maxValue* 0.2,
       // "chardonay": 7,
     },
     {
       skill: "분위기 메이커",
-      chardonay: minMaxScaling(
-        totalChatCount(allCategoryCount) + maxValue * 0.3
+      chardonay:logScaling(
+        totalChatCount(allCategoryCount)
       ),
+    //    minMaxScaling(
+    //     totalChatCount(allCategoryCount) + maxValue * 0.3
+    //   ),
       // totalChatCount(allCategoryCount)+ maxValue* 0.2,
       // "chardonay": 10,
     },
     {
       skill: "협업 의지",
-      chardonay: minMaxScaling(
-        relevantChatCount(allCategoryCount) + maxValue * 0.3
+      chardonay: logScaling(
+        relevantChatCount(allCategoryCount)
       ),
+    //   minMaxScaling(
+    //     relevantChatCount(allCategoryCount) + maxValue * 0.3
+    //   ),
       // relevantChatCount(allCategoryCount)+ maxValue* 0.2,
       // "chardonay": 7,
     },
     {
       skill: "일정 관리",
-      chardonay: minMaxScaling(taskCount + maxValue * 0.3),
+      chardonay: logScaling(taskCount),
+    //   minMaxScaling(taskCount + maxValue * 0.3),
       // taskCount+ maxValue* 0.2,
       // "chardonay": 8,
     },
